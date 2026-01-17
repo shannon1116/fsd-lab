64bdb7bb36f5248860b1d6416668f6f0757e0752
shannon petkau <spetkau2@rrc.ca>
2026-01-17 13:27:03 -0600

    Created a footer file to store the footer component and added function to show the current date and added added a call to the Footer function in the App.tsx file.

--- lab-1.2/src/App.tsx
+++ lab-1.2/src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import './components/departments'
+import './components/footer'
 import DepartmentsList from './components/departments'
+import Footer from './components/footer'
 
 function App() {
 
   return (
     <>
     <DepartmentsList />
+    <Footer />
     </>
   )
 }
---
+++ lab-1.2/src/components/footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    const d = new Date();
+    return(<footer>
+        <p>Copyright Pixell River Financial {d.toString()}</p>
+        </footer>
+    )
+}
+
+export default Footer;
