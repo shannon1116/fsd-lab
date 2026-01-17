a2dc35913139642918f5aa5fae850ba5c1015a12
shannon petkau <spetkau2@rrc.ca>
2026-01-17 13:22:38 -0600

    Created function to display the employees in their department and added a call to that function to the App.tsx file.

---
+++ lab-1.2/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './components/departments'
+import DepartmentsList from './components/departments'
+
+function App() {
+
+  return (
+    <>
+    <DepartmentsList />
+    </>
+  )
+}
+
+export default App
