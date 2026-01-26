6fb4c022a3ef7b656595b43501cb845c03f2f016
shannon petkau <spetkau2@rrc.ca>
2026-01-26 15:12:55 -0600

    Added validation response and error response to form.

--- src/components/employees/employees.tsx
+++ src/components/employees/employees.tsx
@@ -11,17 +11,28 @@ export function EmployeeForm({ onSubmit }: EmployeeFormProps) {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [department, setDepartment] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
-        if (!firstName || !lastName || !department) return;
-
-        onSubmit(department, { firstName, lastName });
-
-        setFirstName("");
-        setLastName("");
-        setDepartment("");
+        if (!firstName || !lastName || !department) {
+            setError("Fields cannot be empty");
+            setSuccess("")
+            return;
+        } else if (firstName.trim().length < 3 || lastName.trim().length < 3) {
+            setError("Both names must be at least 3 characters");
+            setSuccess("")
+            return;
+        } else {
+            onSubmit(department, { firstName, lastName });
+            setError("");
+            setSuccess("Form is valid!");
+            setFirstName("");
+            setLastName("");
+            setDepartment("");
+        }
     
     };
 
@@ -55,10 +66,20 @@ export function EmployeeForm({ onSubmit }: EmployeeFormProps) {
             <option value="">Select a Department</option>
             <option value="Administration">Administration</option>
             <option value="Audit">Audit</option>
+            <option value="Banking Operations">Banking Operations</option>
+            <option value="Communication">Communications</option>
+            <option value="Corporate Services">Corporate Services</option>
+            <option value="Facilities">Facilities</option>
+            <option value="Financial Services">Financial Services</option>
+            <option value="Human Resources">Human Resources</option>
+            <option value="Information Technology">Information Technology</option>
+            <option value="IT Technician">IT Techician</option>
         </select>
     </label>
     <input type="submit"/>
     </form>
+    {error && <p style={{ color: "red"}}>{error}</p>}
+    {success && <p style={{ color: "green" }}>{success}</p>}
     </>
     );
 }
