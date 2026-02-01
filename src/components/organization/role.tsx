a58c7118999dda52823e85ff0eec8616c527632b
shannon petkau <spetkau2@rrc.ca>
2026-02-01 10:15:59 -0600

    Created role file that holds the leadership and management data for Pixell River Financial.

---
+++ src/components/organization/role.tsx
@@ -0,0 +1,247 @@
+const Role = [
+    {
+        role: "CEO/Chair of Board",
+        employees: [
+            {
+                firstName: "Jo-Anne",
+                lastName: "Sinclair",
+                department: "Executive"
+            }
+        ]
+    },
+    {
+        role: "COO/VP Operations",
+        employees: [
+            {
+                firstName: "Jackson",
+                lastName: "Smith",
+                department: "Operations"
+            }
+        ]
+    },
+    {
+        role: "CFO/VP Administration",
+        employees: [
+            {
+                firstName: "Susan",
+                lastName: "Thomas",
+                department: "Administration"
+            }
+        ]
+    },
+    {
+        role: "VP Client Services",
+        employees: [
+            {
+                firstName: "Richa",
+                lastName: "Kaur",
+                department: "Client Services"
+            }
+        ]
+    },
+    {
+        role: "CIO",
+        employees: [
+            {
+                firstName: "Josee",
+                lastName: "Benjamin",
+                department: "Information Security"
+            }
+        ]
+    },
+    {
+        role: "VP Sales & Marketing",
+        employees: [
+            {
+                firstName: "Vincent",
+                lastName: "Grey",
+                department: "Sales & Marketing"
+            }
+        ]
+    },
+    {
+        role: "Director",
+        employees: [
+            {
+                firstName: "Rupa",
+                lastName: "Kharki",
+                department: "Financial and Audit Svcs"
+            },
+            {
+                firstName: "Xun",
+                lastName: "Kuang",
+                department: "Human Resources"
+            },
+            {
+                firstName: "Stien",
+                lastName: "Pedersen",
+                department: "Legal Services/General Counsel"
+            },
+            {
+                firstName: "Sandra",
+                lastName: "Bear",
+                department: "Information Technology"
+            },
+            {
+                firstName: "Gus",
+                lastName: "Blue",
+                department: "Information Security and CISSO"
+            },
+            {
+                firstName: "Sam",
+                lastName: "Kong",
+                department: "Accounting"
+            },
+            {
+                firstName: "Valentine",
+                lastName: "Smith",
+                department: "Physical Security"
+            },
+            {
+                firstName: "Mariya",
+                lastName: "Kaperski",
+                department: "Facilities"
+            }
+        ]
+    },
+    {
+        role: "Manager",
+        employees: [
+            {
+                firstName: "Abd",
+                lastName: "al-Hamid Alami",
+                department: "Business Continuity and Disaster Recovery"
+            },
+            {
+                firstName: "Victoria",
+                lastName: "Gray",
+                department: "Internal Audit"
+            },
+            {
+                firstName: "Jean",
+                lastName: "Ngoy",
+                department: "Security Architecture"
+            },
+            {
+                firstName: "Isaac",
+                lastName: "Smith",
+                department: "Application Solutions"
+            },
+            {
+                firstName: "Samantha",
+                lastName: "Nettle",
+                department: "Operational Risk"
+            },
+            {
+                firstName: "Yolanda",
+                lastName: "Ferreira",
+                department: "Vendor Relations"
+            },
+            {
+                firstName: "Samir",
+                lastName: "Hassan",
+                department: "Purchasing"
+            },
+            {
+                firstName: "Yuna",
+                lastName: "Aikawa",
+                department: "Communications"
+            },
+            {
+                firstName: "Jonathan",
+                lastName: "Carberry",
+                department: "Customer Experience and Community Eng."
+            },
+            {
+                firstNames: "Roland",
+                lastName: "Wei",
+                department: "Sales"
+            },
+            {
+                firstName: "Pran",
+                lastName: "Singh",
+                department: "Marketing"
+            },
+            {
+                firstName: "Esra",
+                lastName: "Sedge",
+                role: "Contract Management"
+            },
+            {
+                firstName: "Pranee",
+                lastName: "Tan",
+                role: "Compliance Management"
+            },
+            {
+                firstName: "Karmen",
+                lastName: "Spruce",
+                department: "IT End User Service Desk"
+            },
+            {
+                firstName: "Hayder",
+                lastName: "Katirci",
+                department: "IT End User Computing"
+            },
+            {
+                firstName: "Jill",
+                lastName: "Harkness",
+                department: "IT Telecom and Infrastructure"
+            },
+            {
+                firstName: "Tim",
+                lastName: "Morrison",
+                department: "Data Center and Hosting Services"
+            },
+            {
+                firstName: "Aleksandr",
+                lastName: "Milosevic",
+                department: "IT Risk Management"
+            },
+            {
+                firstName: "Jim",
+                lastName: "Wingnut",
+                department: "Project Management Office"
+            }
+        ]
+    },
+    {
+        role: "Chief Architect",
+        employees: [
+            {
+                firstName: "Cheryl",
+                lastName: "Guru",
+                department: "Architect"
+            }
+        ]
+    },
+    {
+        role: "Solution Architect",
+        employees: [
+            {
+                firstName: "Kris",
+                lastName: "Gold",
+                department: "Online Banking"
+            }
+        ]
+    },
+    {
+        role: "Lead Developer",
+        employees: [
+            {
+                firstName: "Payton",
+                lastName: "Frost",
+                department: "Online Banking"
+            }
+        ]
+    },
+    {
+        role: "Business Analyst",
+        employees: [
+            {
+                firstName: "Linda",
+                lastName: "Analyst",
+                department: "Online Banking"
+            }
+        ]   
+    }
+]
